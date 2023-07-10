47443ad3e16ec214f98b9bbf08726987517e8609
Volodymyr Dziuba <volodymyr.d@radency.com>
2023-07-10 15:34:01 +0300

    add queries and mutations

---
+++ src/graphql/mutation.ts
@@ -0,0 +1,24 @@
+import { gql } from "@apollo/client"
+
+export const ADD_REPO_TO_FAVORITES = gql`
+  mutation AddRepoToFavorites($repo: Repo!) {
+    addRepoToFavorites(repo: $repo) @client {
+      id
+      name
+      description
+      rating
+    }
+  }
+`
+
+export const REMOVE_REPO_FROM_FAVORITES = gql`
+  mutation RemoveRepoFromFavorites($id: ID!) {
+    removeRepoFromFavorites(id: $id) @client
+  }
+`
+
+export const RATE_FAVORITE_REPO = gql`
+  mutation RateFavoriteRepo($id: ID!, $rating: Int) {
+    rateFavoriteRepo(id: $id, rating: $rating) @client
+  }
+`
---
+++ src/graphql/query.ts
@@ -0,0 +1,42 @@
+import { gql } from "@apollo/client"
+
+export const GET_REPOS_FROM_GITHUB = gql`
+  query listRepos($queryString: String!) {
+    rateLimit {
+      cost
+      remaining
+      resetAt
+    }
+    search(query: $queryString, type: REPOSITORY, first: 20) {
+      repositoryCount
+      pageInfo {
+        endCursor
+        startCursor
+      }
+      edges {
+        node {
+          ... on Repository {
+            id
+            name
+            createdAt
+            description
+            isArchived
+            isPrivate
+            url
+          }
+        }
+      }
+    }
+  }
+`
+
+export const GET_FAVORITE_REPOS_LIST = gql`
+  query GetFavoriteReposList {
+    favoriteRepos @client {
+      id
+      name
+      description
+      rating
+    }
+  }
+`
